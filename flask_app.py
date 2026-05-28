b3ece42174b2ef8071f336d69ceb055b45da62bd
Naatss21 <nathanvncnt21@gmail.com>
2026-05-28 15:37:55 +0200

    ajout web

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API password checker OK"
+    return """
+    <h1>Password Checker</h1>
+    <input id="pwd" type="text" placeholder="mot de passe">
+    <button onclick="send()">Tester</button>
+    <pre id="result"></pre>
+
+    <script>
+    async function send() {
+        const password = document.getElementById("pwd").value;
+
+        const res = await fetch("/check-password", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({password})
+        });
+
+        const data = await res.json();
+        document.getElementById("result").innerText =
+            JSON.stringify(data, null, 2);
+    }
+    </script>
+    """
 
 @app.route("/check-password", methods=["POST"])
 def check_password():
